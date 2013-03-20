46808ebd6dec1fa159ce29df33081e2a1d284498
Mike Spainhower <michael@personal.com>
2013-03-20 16:09:50 -0400

    PT-12121 clear $scope

--- js/jquery.fileupload.js
+++ js/jquery.fileupload.js
@@ -1113,6 +1113,7 @@
             var options = this.options;
             // Initialize options set via HTML5 data-attributes:
             $.extend(options, $(this.element[0].cloneNode(false)).data());
+            options.$scope = null;
             this._initSpecialOptions();
             this._slots = [];
             this._sequence = this._getXHRPromise(true);
